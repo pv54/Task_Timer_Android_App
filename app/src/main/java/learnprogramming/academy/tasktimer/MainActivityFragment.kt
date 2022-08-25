bddfea1326c49e6ba1ab50830834d9d46cc064d3
PRASHANT VERMA <pv549867@gmail.com>
2022-08-25 23:01:38 +0530

    first commit

---
+++ app/src/main/java/learnprogramming/academy/tasktimer/MainActivityFragment.kt
@@ -0,0 +1,180 @@
+package learnprogramming.academy.tasktimer
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+private const val TAG = "MainActivityFragment"
+private const val DIALOG_ID_DELETE = 1
+private const val DIALOG_TASK_ID = "task_id"
+private const val DIALOG_TASK_POSITION = "task_position"
+
+class MainActivityFragment : Fragment(),
+        CursorRecyclerViewAdapter.OnTaskClickListener,
+        AppDialog.DialogEvents {
+
+    interface OnTaskEdit {
+        fun onTaskEdit(task: Task)
+    }
+
+    //    private val viewModel by lazy { ViewModelProvider(activity!!).get(TaskTimerViewModel::class.java) }
+    private val viewModel: TaskTimerViewModel by activityViewModels()
+
+    private val mAdapter = CursorRecyclerViewAdapter(null, this)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "onCreateView: called")
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG, "onAttach: called")
+        super.onAttach(context)
+
+        if (context !is OnTaskEdit) {
+            throw RuntimeException("$context must implement OnTaskEdit")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
+        super.onCreate(savedInstanceState)
+        viewModel.cursor.observe(this, Observer { cursor -> mAdapter.swapCursor(cursor)?.close() })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewCreated: called")
+        super.onViewCreated(view, savedInstanceState)
+
+        task_list.layoutManager = LinearLayoutManager(context)    // <-- set up RecyclerView
+        task_list.adapter = mAdapter
+
+        val itemTouchHelper = ItemTouchHelper(
+                object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+                    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                        // Implement this to allow sorting the tasks by dragging them up and down in the list.
+                        return false  // (return true if you move an item)
+                    }
+
+                    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                        Log.d(TAG, "onSwiped called")
+                        if (direction == ItemTouchHelper.LEFT) {
+                            val task = (viewHolder as TaskViewHolder).task
+                            if (task.id == viewModel.editedTaskId) {
+                                mAdapter.notifyItemChanged(viewHolder.adapterPosition)
+                                Toast.makeText(context, getString(R.string.delete_edited_task), Toast.LENGTH_SHORT).show()
+                            } else {
+                                onDeleteClick(task, viewHolder.adapterPosition)
+                            }
+                        }
+                    }
+                })
+
+        itemTouchHelper.attachToRecyclerView(task_list)
+    }
+
+    override fun onEditClick(task: Task) {
+        (activity as OnTaskEdit?)?.onTaskEdit(task)
+    }
+
+    fun onDeleteClick(task: Task, position: Int) {
+        val args = Bundle().apply {
+            putInt(DIALOG_ID, DIALOG_ID_DELETE)
+            putString(DIALOG_MESSAGE, getString(R.string.deldiag_message, task.id, task.name))
+            putInt(DIALOG_POSITIVE_RID, R.string.deldiag_positive_caption)
+            putLong(DIALOG_TASK_ID, task.id)   // pass the id in the arguments, so we can retrieve it when we get called back.
+            putInt(DIALOG_TASK_POSITION, position)
+        }
+        val dialog = AppDialog()
+        dialog.arguments = args
+        dialog.show(childFragmentManager, null)
+    }
+
+    override fun onTaskLongClick(task: Task) {
+        Log.d(TAG, "onTaskLongClick: called")
+        viewModel.timeTask(task)
+    }
+
+    override fun onPositiveDialogResult(dialogId: Int, args: Bundle) {
+        Log.d(TAG, "onPositiveDialogResult: called with id $dialogId")
+
+        if (dialogId == DIALOG_ID_DELETE) {
+            val taskId = args.getLong(DIALOG_TASK_ID)
+            if (BuildConfig.DEBUG && taskId == 0L) throw AssertionError("Task ID is zero")
+            viewModel.deleteTask(taskId)
+        }
+    }
+
+    override fun onNegativeDialogResult(dialogId: Int, args: Bundle) {
+        Log.d(TAG, "onNegativeDialogResult called")
+        if (dialogId == DIALOG_ID_DELETE) {
+            val position = args.getInt(DIALOG_TASK_POSITION)
+            Log.d(TAG, "onNegativeDialogResult restoring item at position $position")
+            // Update the adapter
+            mAdapter.notifyItemChanged(position)
+        }
+    }
+
+    // TODO: Delete all these functions before release.
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewStateRestored: called")
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView: called")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach: called")
+        super.onDetach()
+    }
+}
---
+++ app/src/main/java/learnprogramming/academy/tasktimer/TaskTimerViewModel.kt
@@ -0,0 +1,246 @@
+package learnprogramming.academy.tasktimer
+
+import android.app.Application
+import android.content.ContentValues
+import android.content.SharedPreferences
+import android.database.ContentObserver
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
+import androidx.preference.PreferenceManager
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+
+import kotlinx.coroutines.launch
+
+/**
+ * Created by timbuchalka for the Android Pie using Kotlin course
+ * from www.learnprogramming.academy
+ */
+
+private const val TAG = "TaskTimerViewModel"
+
+class TaskTimerViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val contentObserver = object : ContentObserver(Handler()) {
+        override fun onChange(selfChange: Boolean, uri: Uri?) {
+            Log.d(TAG, "contentObserver.onChange: called. uri is $uri")
+            loadTasks()
+        }
+    }
+
+    private val settings = PreferenceManager.getDefaultSharedPreferences(application)
+    private var ignoreLessThan = settings.getInt(SETTINGS_IGNORE_LESS_THAN, SETTINGS_DEFAULT_IGNORE_LESS_THAN)
+
+    private val settingsListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+        when (key) {
+            SETTINGS_IGNORE_LESS_THAN -> {
+                ignoreLessThan = sharedPreferences.getInt(key, SETTINGS_DEFAULT_IGNORE_LESS_THAN)
+                Log.d(TAG, "settingsListener: now ignoring timings less than $ignoreLessThan seconds")
+
+                // Update the database parameters table
+                val values = ContentValues()
+                values.put(ParametersContract.Columns.VALUE, ignoreLessThan)
+                viewModelScope.launch {
+                    Dispatchers.IO
+                    val uri = getApplication<Application>().contentResolver
+                            .update(ParametersContract.buildUriFromId(ParametersContract.ID_SHORT_TIMING),
+                                    values,
+                                    null,
+                                    null)
+                }
+            }
+        }
+    }
+
+    private var currentTiming: Timing? = null
+
+    private val databaseCursor = MutableLiveData<Cursor>()
+    val cursor: LiveData<Cursor>
+        get() = databaseCursor
+
+    private val taskTiming = MutableLiveData<String>()
+    val timing: LiveData<String>
+        get() = taskTiming
+
+    var editedTaskId = 0L
+        private set
+
+    init {
+        Log.d(TAG, "TaskTimerViewModel: created")
+        getApplication<Application>().contentResolver.registerContentObserver(TasksContract.CONTENT_URI,
+                true, contentObserver)
+
+        Log.d(TAG, "Ignoring timings less than $ignoreLessThan seconds")
+        settings.registerOnSharedPreferenceChangeListener(settingsListener)
+
+        currentTiming = retrieveTiming()
+        loadTasks()
+    }
+
+    private fun loadTasks() {
+        val projection = arrayOf(TasksContract.Columns.ID,
+                TasksContract.Columns.TASK_NAME,
+                TasksContract.Columns.TASK_DESCRIPTION,
+                TasksContract.Columns.TASK_SORT_ORDER)
+        // <order by> Tasks.SortOrder, Tasks.Name COLLATE NOCASE
+        val sortOrder = "${TasksContract.Columns.TASK_SORT_ORDER}, ${TasksContract.Columns.TASK_NAME} COLLATE NOCASE"
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val cursor = getApplication<Application>().contentResolver.query(
+                    TasksContract.CONTENT_URI,
+                    projection, null, null,
+                    sortOrder)
+            databaseCursor.postValue(cursor)
+        }
+    }
+
+    fun saveTask(task: Task): Task {
+        val values = ContentValues()
+
+        if (task.name.isNotEmpty()) {
+            // Don't save a task wth no name
+            values.put(TasksContract.Columns.TASK_NAME, task.name)
+            values.put(TasksContract.Columns.TASK_DESCRIPTION, task.description)
+            values.put(TasksContract.Columns.TASK_SORT_ORDER, task.sortOrder)  // defaults to zero if empty
+
+            if (task.id == 0L) {
+                viewModelScope.launch(Dispatchers.IO) {
+                    Log.d(TAG, "saveTask: adding new task")
+                    val uri = getApplication<Application>().contentResolver?.insert(TasksContract.CONTENT_URI, values)
+                    if (uri != null) {
+                        task.id = TasksContract.getId(uri)
+                        Log.d(TAG, "saveTask: new id is ${task.id}")
+                    }
+                }
+            } else {
+                // task has an id, so we're updating
+                viewModelScope.launch(Dispatchers.IO) {
+                    Log.d(TAG, "saveTask: updating task")
+                    getApplication<Application>().contentResolver?.update(TasksContract.buildUriFromId(task.id), values, null, null)
+                }
+            }
+        }
+        return task
+    }
+
+    fun startEditing(taskId: Long) {
+        if (BuildConfig.DEBUG && editedTaskId != 0L) throw IllegalStateException(
+                "startEditing called without stopping previous edit. editedTaskId: $editedTaskId, taskId: $taskId"
+        )
+        editedTaskId = taskId
+    }
+
+    fun stopEditing() {
+        editedTaskId = 0L
+    }
+
+    fun deleteTask(taskId: Long) {
+        Log.d(TAG, "Deleting task")
+        viewModelScope.launch(Dispatchers.IO) {
+            getApplication<Application>().contentResolver?.delete(TasksContract.buildUriFromId(taskId), null, null)
+        }
+    }
+
+    fun timeTask(task: Task) {
+        Log.d(TAG, "timeTask: called")
+        // Use local variable, to allow smart casts
+        val timingRecord = currentTiming
+
+        if (timingRecord == null) {
+            // no task being timed, start timing the new task
+            currentTiming = Timing(task.id)
+            saveTiming(currentTiming!!)
+        } else {
+            // We have a task being timed, so save it
+            timingRecord.setDuration()
+            saveTiming(timingRecord)
+
+            currentTiming = if (task.id == timingRecord.taskId) {
+                // the current task was tapped a second time, stop timing
+                null
+            } else {
+                // a new task is being timed
+                val newTiming = Timing(task.id)
+                saveTiming(newTiming)
+                newTiming
+            }
+        }
+
+        // Update the LiveData
+        taskTiming.value = if (currentTiming != null) task.name else null
+    }
+
+    private fun saveTiming(currentTiming: Timing) {
+        Log.d(TAG, "saveTiming: called")
+
+        // Are we updating, or inserting a new row?
+        val inserting = (currentTiming.duration == 0L)
+
+        val values = ContentValues().apply {
+            if (inserting) {
+                put(TimingsContract.Columns.TIMING_TASK_ID, currentTiming.taskId)
+                put(TimingsContract.Columns.TIMING_START_TIME, currentTiming.startTime)
+            }
+            put(TimingsContract.Columns.TIMING_DURATION, currentTiming.duration)
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            if (inserting) {
+                val uri = getApplication<Application>().contentResolver.insert(TimingsContract.CONTENT_URI, values)
+                if (uri != null) {
+                    currentTiming.id = TimingsContract.getId(uri)
+                }
+            } else {
+
+                Log.d(TAG, "saveTiming: saving timing with duration ${currentTiming.duration}")
+                getApplication<Application>().contentResolver.update(TimingsContract.buildUriFromId(currentTiming.id), values, null, null)
+            }
+        }
+    }
+
+    private fun retrieveTiming(): Timing? {
+        Log.d(TAG, "retrieveTiming starts")
+        val timing: Timing?
+
+        val timingCursor: Cursor? = getApplication<Application>().contentResolver.query(
+                CurrentTimingContract.CONTENT_URI,
+                null,  // passing null for the projection returns all columns.
+                null,
+                null,
+                null)
+
+        if (timingCursor != null && timingCursor.moveToFirst()) {
+            // We have an un-timed record
+            val id = timingCursor.getLong(timingCursor.getColumnIndex(CurrentTimingContract.Columns.TIMING_ID))
+            val taskId = timingCursor.getLong(timingCursor.getColumnIndex(CurrentTimingContract.Columns.TASK_ID))
+            val startTime = timingCursor.getLong(timingCursor.getColumnIndex(CurrentTimingContract.Columns.START_TIME))
+            val name = timingCursor.getString(timingCursor.getColumnIndex(CurrentTimingContract.Columns.TASK_NAME))
+            timing = Timing(taskId, startTime, id)
+
+            // Update the LiveData
+            taskTiming.value = name
+
+        } else {
+            // No timing record found with zero duration
+            timing = null
+        }
+
+        timingCursor?.close()
+
+        Log.d(TAG, "retrieveTiming returning")
+        return timing
+    }
+
+    override fun onCleared() {
+        Log.d(TAG, "onCleared: called")
+        getApplication<Application>().contentResolver.unregisterContentObserver(contentObserver)
+
+        settings.unregisterOnSharedPreferenceChangeListener(settingsListener)
+
+    }
+}
